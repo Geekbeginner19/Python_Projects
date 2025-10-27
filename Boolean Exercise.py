6514b1fcf591818190570e1d9a580d7b39e08cb0
Kelvin_Ofori_Amoafo <86635380+Geekbeginner19@users.noreply.github.com>
2025-10-27 09:40:27 +0000

    Small exercise on the implementation of boolean

--- Boolean Exercise.py
+++ Boolean Exercise.py
@@ -9,8 +9,26 @@ PRINT WHETHER
 #STORING THE AGE IN A VARIABLE
 KelvinAge = 23
 
-#GETTING INTO THE CONDITIONS
-userAge = int(input("Please enter your age: "))
+#COLLECTING USER INFO
+userAge = input("Please enter your age: ")
+#Handling an error where user enters a string
+while True:
+    try:
+        userAge = int(userAge)
+        #Validating the data whether it's within the valid range
+        dataValid = False
+        while dataValid == False:
+            if userAge < 0:
+                userAge = int(input("Please enter a valid age: "))
+            else:
+                dataValid = True
+                continue #skips into the inner while True loop
+        break #jumps out of the Outta while True loop
+    except:
+        userAge = input("Please enter a number: ")
+        continue #goes back to execute the loop until the condition is met
+
+#HANDLING CONDITIONS
 if (userAge > KelvinAge):
     print("You're Older than Kelvin")
 elif (userAge < KelvinAge):
