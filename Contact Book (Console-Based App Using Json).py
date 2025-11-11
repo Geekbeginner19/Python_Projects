c387bb990ca23c8a94bf38c26ec2226bf25adb46
Kelvin_Ofori_Amoafo <86635380+Geekbeginner19@users.noreply.github.com>
2025-11-11 18:51:47 +0000

    Update Contact Book (Console-Based App Using Json).py

--- Contact Book (Console-Based App Using Json).py
+++ Contact Book (Console-Based App Using Json).py
@@ -36,7 +36,7 @@
 # 5. Quit
 # Exit the program gracefully.
 
-import os
+import os #handling files
 import json
 
 filename = r"C:\Users\Rarissime\Downloads\Contacts.json"
@@ -51,7 +51,7 @@ def add_new_contact():
     contactDict.update({"phone" : phone})
     contactDict.update({"email" : email})
     if os.path.exists(filename):#checking if the json file already exist
-        with open(filename, "r") as file:#reads contacts in file as 'file'
+        with open(filename, "r") as file:#reads contacts in file as 'file'(safely)
             contactList = json.load(file)#loads all contacts into
     else:
         contactList = []
@@ -62,7 +62,7 @@ def add_new_contact():
 
 
 def view_all_contacts():
-    if os.path.exists(filename):
+    if os.path.exists(filename):#checking to see if the file exists
         with open(filename, "r") as file:
             contacts = json.load(file)
         for contact in contacts:
@@ -73,27 +73,64 @@ def view_all_contacts():
     
 
 def search_contact():
-    if os.path.exists(filename):
+    if os.path.exists(filename):#checking to see if the file exists
         search = input("Please enter the name you want to search: ").lower()
-        with open(filename, "r") as file:
-            LoadedContactsList = json.load(file)
+        with open(filename, "r") as file:#Best way top open and close json files to avoid corruption
+            contactList = json.load(file)
         found = False
-        for contact in LoadedContactsList:
+        for contact in contactList:#go through each dictionary in the list
             if search.lower() == contact['name'].lower():
                 print(f"{contact['name']} | {contact['phone']} | {contact['email']}")
                 found = True
             else:
                 continue
-        if not found:
+        if not found:#if no contact matches search
             print("Couldn't find a match.")
     else:
         print("No contacts to View")
 
 
-#def delete_contact():
-    #to be continued
+def delete_contact():
+    if os.path.exists(filename):#checking to see if the file exists
+        search = input("Please enter the name you want to delete: ").lower()
+        with open(filename, "r") as file:#Best way top open and close json files to avoid corruption
+            contactList = json.load(file)
+        found = False
+        for contact in contactList:#go through each dictionary in the list
+            if search.lower() == contact['name'].lower():
+                contactList.remove(contact) 
+                found = True
+                break #stopping the loop after the first found contact matching the description has been removed (helps to prevents unnecessary removal of duplicates)            
+            else:
+                continue
+        if not found:#if no contact matches search
+            print("Couldn't find a match.")
+            
+        with open(filename, "w") as file:#safe handling of files 
+            #Saving the updated list into the json file
+            json.dump(contactList, file, indent = 4)#Converting or writing the list into a json file (Contacts.json) indent = 4 fives it some nice indentaation
+    else:
+        print("No contacts to View")
+    
+while True:
+    options = input("\n--- Contact Book ---\n1. Add a new contact\n2. View all contacts\n3. Search for a contact\n4. Delete a contact\n5. Quit\nChoose an option>>> ")
+    try:#error handling
+        options = int(options)
+        if options < 1 or options > 5:#chaecking if the options is outta the range
+            print("Please enter a valid range of options")
+        else:
+            if options == 1:#add a new contact
+                add_new_contact()
+            elif(options == 2):#view all contacts
+                view_all_contacts()
+            elif(options == 3):#Serach for a contact
+                search_contact()
+            elif(options == 4):#delete a contact
+                delete_contact()
+            elif(options == 5):#quit the menu
+                print("Goodbye!")
+                break#jump outta the loop if the user wants to quit
+    except ValueError as v:
+        print(f"Please enter a valid option ({v})")
 
 
-# add_new_contact()
-# view_all_contacts()
-search_contact()
