a007b04a2c25e8040a9b7d32a7d09ee8babf0c0f
Kelvin_Ofori_Amoafo <86635380+Geekbeginner19@users.noreply.github.com>
2025-12-05 09:28:20 +0000

    Update Inventory and Stock Management(OOP).py

--- Inventory and Stock Management(OOP).py
+++ Inventory and Stock Management(OOP).py
@@ -19,6 +19,13 @@ class Item:
         self.name = name 
         self.price = price
         self.quantity = quantity
+    
+    def to_dict(self):
+        return {
+            "name" : self.name,
+            "price" : self.price,
+            "quantity" : self.quantity
+        }
 
     def add_stock(self, amount:int):
         if amount > 0:
@@ -35,11 +42,59 @@ class Item:
     def display(self):
         print(f"Stock Name: {self.name} | Price: {self.price} | Quantity: {self.quantity}")
         
-item = Item("Sugar", 35, 200)
-item.add_stock(200)
-item.display()
-item.reduce_stock(350)
-item.display()
+# item = Item("Sugar", 35, 200)
+# item.add_stock(200)
+# item.display()
+# item.reduce_stock(350)
+# item.display()
         
-#Mini Project One done!
-#TO BE CONTINUED
+#Class Composition inventoty to handle multiple item objects
+class Inventory:
+    def __init__(self):
+        self.items = []
+    
+    def add_items(self, item:str):
+        self.items.append(item)
+    
+    def remove_item(self, name:str):
+        for item in self.items:
+            if name.lower() == item.name.lower():
+                self.items.remove(item)
+            else:
+                continue
+
+    def view_all_items(self):
+        for item in self.items:
+            item.display()
+
+    def search(self, name:str):
+        found = False
+        for item in self.items:
+            if name.lower() == item.name.lower():
+                item.display()
+                found = True
+            else:
+                continue
+        if not found:
+            print("No match found")
+    
+    #Update Stock Items
+    def update_item(self, name:str, price = None, quantity = None):
+        for item in self.items:#looping through the item objects
+            if name.lower() == item.name.lower():
+                if price is not None:
+                    item.price = price
+                
+                if quantity is not None:
+                    item.quantity = quantity
+            else:
+                continue
+            print("Successfully Updated Items!")
+
+    
+# inventory = Inventory()
+# inventory.add_items(Item("Sugar", 450, 50))
+# inventory.view_all_items()
+# inventory.update_item("Sugar", 500, 40)
+# inventory.view_all_items()
+
