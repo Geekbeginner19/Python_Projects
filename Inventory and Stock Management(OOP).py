416ba1a6c8a404856ca29c4fdb75cd12829dd67e
Kelvin_Ofori_Amoafo <86635380+Geekbeginner19@users.noreply.github.com>
2025-12-05 12:27:29 +0000

    Update Inventory and Stock Management(OOP).py

--- Inventory and Stock Management(OOP).py
+++ Inventory and Stock Management(OOP).py
@@ -14,6 +14,11 @@
 # Save/load inventory to a JSON file
 # Just like a real mini-POS (Point of Sale) system.
 
+import json
+import os
+
+filePath = r"C:\Users\Rarissime\Downloads\InventoryManager.json"
+
 class Item:
     def __init__(self, name:str, price:float, quantity:int):
         self.name = name 
@@ -33,14 +38,14 @@ class Item:
         else:
             print("Cannot add negative items to the stock")
         
-    def reduce_stock(self, amount):
+    def reduce_stock(self, amount:int):
         if amount > 0 and amount <= self.quantity:
             self.quantity -= amount
         else:
             print("Cannot remove more than stock quantity")
         
     def display(self):
-        print(f"Stock Name: {self.name} | Price: {self.price} | Quantity: {self.quantity}")
+        print(f"Stock Name: {self.name} | Price: ${self.price} | Quantity: {self.quantity}")
         
 # item = Item("Sugar", 35, 200)
 # item.add_stock(200)
@@ -52,49 +57,141 @@ class Item:
 class Inventory:
     def __init__(self):
         self.items = []
+        self.jsonlst = []
     
-    def add_items(self, item:str):
+    def add_items(self, item):
         self.items.append(item)
     
     def remove_item(self, name:str):
-        for item in self.items:
-            if name.lower() == item.name.lower():
-                self.items.remove(item)
-            else:
-                continue
+        if len(self.items) == 0 or not os.path.exists(filePath):
+            print("\nNothing to see here")
+        else:               
+            for item in self.items:
+                if name.lower() == item.name.lower():
+                    self.items.remove(item)
+                else:
+                    continue
 
     def view_all_items(self):
-        for item in self.items:
-            item.display()
+        if len(self.items) == 0 or not os.path.exists(filePath):
+            print("Nothing to see here")
+        else:    
+            for item in self.items:
+                item.display()
 
     def search(self, name:str):
-        found = False
-        for item in self.items:
-            if name.lower() == item.name.lower():
-                item.display()
-                found = True
-            else:
-                continue
-        if not found:
-            print("No match found")
+        if len(self.items) == 0 or not os.path.exists(filePath):
+            print("\nNothing to see here")
+        else:                
+            found = False
+            for item in self.items:
+                if name.lower() in item.name.lower(): #partial matches
+                    item.display()
+                    found = True
+                else:
+                    continue
+            if not found:
+                print("\nNo match found")
     
     #Update Stock Items
     def update_item(self, name:str, price = None, quantity = None):
-        for item in self.items:#looping through the item objects
-            if name.lower() == item.name.lower():
-                if price is not None:
-                    item.price = price
-                
-                if quantity is not None:
-                    item.quantity = quantity
-            else:
-                continue
-            print("Successfully Updated Items!")
-
+        found = False
+        if len(self.items) == 0 or not os.path.exists(filePath):
+            print("\nNo inventory Available to update")
+        else:
+            for item in self.items:#looping through the item objects
+                if name.lower() == item.name.lower():
+                    if price is not None:
+                        item.price = price
+                    
+                    if quantity is not None:
+                        item.quantity = quantity
+                else:
+                    continue
+                print("\nSuccessfully Updated Inventory!") 
+            if not found:
+                print("\nNo match found")
+           
     
-# inventory = Inventory()
+    def dumpjson(self):
+        if os.path.exists(filePath):        
+            self.jsonlst = []  # clear previous json list
+            for item in self.items:
+                self.jsonlst.append(item.to_dict())  # convert each Item object to dict
+            with open(filePath, "w") as file:
+                json.dump(self.jsonlst, file, indent=4)  #save
+            print("\nInventory Saved Successfully!")
+        else:
+            print("\nNo Inventory to save")
+
+    def loadjson(self):
+        if os.path.exists(filePath):#check to see if json file exists
+            with open(filePath, "r") as file:
+                loaded_list = json.load(file)  # load list of dicts from JSON
+            self.items = []  # clear current items
+            for data in loaded_list:
+                item_obj = Item(data["name"], data["price"], data["quantity"])  # convert dict back to Item Object
+                self.items.append(item_obj)#Adds the objects back into the self.items list
+            print("\nInventory loaded successfully!")
+        else:
+            print("\nNo inventory to see")
+                
+    
+inventory = Inventory()
 # inventory.add_items(Item("Sugar", 450, 50))
 # inventory.view_all_items()
 # inventory.update_item("Sugar", 500, 40)
 # inventory.view_all_items()
+# inventory.dumpjson()  # save inventory
+# inventory.loadjson()  # load inventory and rebuild Item objects
 
+def addItems():
+    name = input("Enter name: ")
+    price = float(input("Enter price: "))
+    quantity = int(input("Enter quantity: "))
+    return Item(name, price, quantity)
+
+while True:
+    print("\n=== Stock and Inventory Management System ===")
+    print("1. Add item")
+    print("2. View All Items")
+    print("3. Remove Item")
+    print("4. Search Item")
+    print("5. Update Item")
+    print("6. Save Inventory")
+    print("7. Load Inventory")
+    print("8. Quit\n")
+    option = input("Choose an option >>> ")
+
+    try:
+        option = int(option)
+        if option < 1 or option > 8:
+            print("Please enter a valid option")
+        else:
+            if option == 1:
+                add = addItems()
+                inventory.add_items(add)
+                inventory.dumpjson() # saves after inventory is added 
+            elif (option == 2):
+                inventory.view_all_items()
+            elif(option == 3):
+                name = input("Enter item name: ")
+                inventory.remove_item(name)
+            elif(option == 4):
+                name = input("Enter item name: ")
+                inventory.search(name)
+            elif(option == 5):
+                name = input("Enter item name: ")
+                price = float(input("Enter price: "))
+                quantity = int(input("Enter quantity: "))
+                inventory.update_item(name, price, quantity)
+            elif(option == 6):
+                inventory.dumpjson()
+            elif(option == 7):
+                inventory.loadjson()
+            elif(option == 8):
+                print("Goodbye!\n")
+                break
+    except ValueError as V:
+        print(f"Invalid option entered {V}")
+    
